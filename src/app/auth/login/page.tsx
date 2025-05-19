6b967366a70a89f521f51a992d2f796a088e30b8
Sudarshan Gadekar <sudarshangadekar222@gmail.com>
2025-05-20 00:48:05 +0530

    add registration page ssr

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,16 +1,16 @@
 "use client"
-// import dynamic from 'next/dynamic';
-// import LoginPageSkeleton from '@/components/LoginPageSkelaton';
+import dynamic from 'next/dynamic';
+import LoginPageSkeleton from '@/components/LoginPageSkelaton';
 
-// const LoginPage = dynamic(() => import('@/components/LoginPage'), {
-//   ssr:false,
-//   loading: () => <LoginPageSkeleton />,
-// });
+const LoginPage = dynamic(() => import('@/components/LoginPage'), {
+  ssr:false,
+  loading: () => <LoginPageSkeleton />,
+});
 export default function Login() {
   return (
     <div className="p-6">
-      this is login page
-      {/* <LoginPage/> */}
+
+      <LoginPage/>
     </div>
   );
 }
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,6 +1,12 @@
+"use client"
+// import RegisterPage from '@/components/RegisterPage';
+import dynamic from 'next/dynamic';
+import LoginPageSkeleton from '@/components/LoginPageSkelaton';
 
-import RegisterPage from '@/components/RegisterPage';
-
+const RegisterPage = dynamic(() => import('@/components/RegisterPage'), {
+  ssr:false,
+  loading: () => <LoginPageSkeleton />,
+});
 export default function CreatePropertyPage() {
   return (
     <div className="p-6">
