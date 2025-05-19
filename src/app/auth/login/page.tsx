77d6cafd1cb6cc2556359866dc9e615358907f20
Sudarshan Gadekar <sudarshangadekar222@gmail.com>
2025-05-20 00:39:40 +0530

    comment login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,11 +1,11 @@
 "use client"
-import dynamic from 'next/dynamic';
-import LoginPageSkeleton from '@/components/LoginPageSkelaton';
+// import dynamic from 'next/dynamic';
+// import LoginPageSkeleton from '@/components/LoginPageSkelaton';
 
-const LoginPage = dynamic(() => import('@/components/LoginPage'), {
-  ssr:false,
-  loading: () => <LoginPageSkeleton />,
-});
+// const LoginPage = dynamic(() => import('@/components/LoginPage'), {
+//   ssr:false,
+//   loading: () => <LoginPageSkeleton />,
+// });
 export default function Login() {
   return (
     <div className="p-6">
