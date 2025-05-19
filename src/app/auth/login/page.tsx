7de98b6141d8ff4dcdc8b1c45e5201d0713fa0d5
Sudarshan Gadekar <sudarshangadekar222@gmail.com>
2025-05-20 00:36:49 +0530

    add use client

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,15 +1,16 @@
-
+"use client"
 import dynamic from 'next/dynamic';
 import LoginPageSkeleton from '@/components/LoginPageSkelaton';
 
 const LoginPage = dynamic(() => import('@/components/LoginPage'), {
+  ssr:false,
   loading: () => <LoginPageSkeleton />,
 });
 export default function Login() {
   return (
     <div className="p-6">
-
-      <LoginPage/>
+      this is login page
+      {/* <LoginPage/> */}
     </div>
   );
 }
